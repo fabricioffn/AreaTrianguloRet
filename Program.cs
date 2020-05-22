b6ddaa5cd42202d2f8e3bc75f8df0972ece4bcd3
fabricio <fnmamao@gmail.com>
2020-05-22 10:02:38 -0300

    adiciona exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double b;
+            Double h;
+            Console.WriteLine("Calcule a área de um triângulo retângulo.");
+            Console.WriteLine("informe a base do triângulo:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("informe a altura do triângulo:");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            //Conta
+            Console.WriteLine("Área = " + b*h/2);
+        }
+    }
+}
